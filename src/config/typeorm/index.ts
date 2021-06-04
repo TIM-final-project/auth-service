c8c991fdc270a1e92afaf342cdb29a8cd177adac
Tomas Luna <tomas_luna1996@hotmail.com>
2021-06-04 00:23:05 -0300

    first commit

---
+++ src/config/typeorm/index.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common'
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { getMetadataArgsStorage } from 'typeorm'
+import { TYPEORM } from '../../environments'
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+	async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
+		return {
+			...TYPEORM,
+			type: 'mysql',
+			entities: ['dist/**/*.entity{ .ts,.js}'],
+			synchronize: true,
+            autoLoadEntities: true,
+			keepConnectionAlive: true,
+			logging: true
+		};
+	}
+}
---
+++ src/users/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private customerRepository: Repository<User>,
+      ) {}
+
+      create(data: any): Promise<User>{
+          return this.customerRepository.save(data);
+      }
+    
+      findAll(): Promise<User[]> {
+        return this.customerRepository.find();
+      }
+    
+      findOne(id: string): Promise<User> {
+        return this.customerRepository.findOne(id);
+      }
+}
