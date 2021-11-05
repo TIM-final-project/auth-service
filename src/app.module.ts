1cb5779be5d9fc2ecb83a103e6703ddfc1c0d748
Tomas Luna <tomas_luna1996@hotmail.com>
2021-11-05 01:07:27 -0300

    reorder typeorm configuration for be aware of env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmConfigService } from './config';
 import { UsersModule } from './user/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmConfigService } from './config/typeorm';
 
 @Module({
   imports: [
--- src/environments/index.ts
+++ src/environments/index.ts
@@ -3,34 +3,36 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 // environment
-const NODE_ENV: string = process.env.NODE_ENV || 'local';
+const NODE_ENV: string = process.env.NODE_ENV || 'dev';
 
 //application
-const PORT: number = +process.env.PORT || 14047;
+const PORT: number = +process.env.PORT || 3001;
 
 //jwt
 const JWT_SECRET_KEY: string = process.env.JWT_SECRET_KEY;
 
 // typeorm
-const environment = {
-  local: {
+const typeorm_default = {
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+}
+
+const typeorm_conf = {
+  dev: {
+    ...typeorm_default,
+    syncronize: true,
+    logging: true,
   },
-  // development: {
-  //     host: 'localhost',
-  // 	port: MONGO_PORT!,
-  // 	username: '',
-  // 	password: '',
-  // 	database: MONGO_DB!,
-  // },
-  // production: {
-  // 	url: MLAB_URL
-  // }
+  production: {
+    ...typeorm_default,
+    syncronize: false,
+    logging: false,
+  },
 };
-const TYPEORM = environment[NODE_ENV];
+
+const TYPEORM = typeorm_conf[NODE_ENV];
 
 export { NODE_ENV, PORT, TYPEORM, JWT_SECRET_KEY as JWT_SECRET_JEY };
